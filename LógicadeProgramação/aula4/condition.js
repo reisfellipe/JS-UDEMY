f5e8531b9a215720011b74cee7feeeb0c4f064ac
Fellipe <reisfellipe11@gmail.com>
2023-11-30 00:34:26 -0300

    testing the if else

---
+++ LógicadeProgramação/aula4/condition.js
@@ -0,0 +1,11 @@
+const hora = 8;
+
+if(hora < 12) {
+    console.log('GOOD MORNING, Seja bem vindo');
+} else if (hora >= 12 && hora <= 17) {
+    console.log('GOOD AFTERNOON, Seja bem vindo');
+} else if (hora >= 18 && hora <= 23){
+    console.log('Good Evening, Seja bem vindo');
+} else{
+    console.log('Seja bem vindo')
+}
