89c0f3892e8f8566cacbdfb2f3b41a8d89cd4ecb
Fellipe <reisfellipe11@gmail.com>
2023-11-30 00:41:50 -0300

    saving the progress

--- LógicadeProgramação/aula4/condition.js
+++ LógicadeProgramação/aula4/condition.js
@@ -1,11 +1,20 @@
+/*
+if Pode ser usado sozinho
+Posso ter vários else if´s na checagem
+Só posso ter um else na checagem
+Podemos usar condições sem else if
+*/
+
+
 const hora = 8;
 
 if(hora < 12) {
-    console.log('GOOD MORNING, Seja bem vindo');
+    console.log('Good Morning, Seja bem vindo');
 } else if (hora >= 12 && hora <= 17) {
-    console.log('GOOD AFTERNOON, Seja bem vindo');
+    console.log('Good Afternoon, Seja bem vindo');
 } else if (hora >= 18 && hora <= 23){
     console.log('Good Evening, Seja bem vindo');
 } else{
     console.log('Seja bem vindo')
 }
+
