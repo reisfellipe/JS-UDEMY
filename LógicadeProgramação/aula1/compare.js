f1a139bce309ad31673a1468048e21dcefbaf19e
Fellipe <reisfellipe11@gmail.com>
2023-11-29 23:32:55 -0300

    creating new files

---
+++ LógicadeProgramação/aula1/compare.js
@@ -0,0 +1,13 @@
+/*
+Operadores de comparação
+> maior que
+< menor que
+>= maior que ou igual a
+<= menor que ou igual a
+== igualdade (valor)  NÃO RECOMENDADO
+=== igualdade estrita (valor e tipo)
+!= diferente (valor)  NÃO RECOMENDADO
+!== diferente estrito (valor e tipo)
+*/
+
+console.log(10 > 5); //true
