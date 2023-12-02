45ac46f0cab508e3387beafde2016bbe9d92e124
Fellipe <reisfellipe11@gmail.com>
2023-12-01 23:43:18 -0300

    increasing exercises

--- LógicadeProgramação/aula8/index.js
+++ LógicadeProgramação/aula8/index.js
@@ -1,14 +1,32 @@
 const verdadeiro = true;
 
-let nome = 'Reis'; //Variável global, porém let é variável de escopo de bloco
-var nome1 = 'Silva'; 
+// let nome = 'Reis'; //Variável global, porém let é variável de escopo de bloco
+// var nome1 = 'Silva'; 
 
-if (verdadeiro) {
-    let nome = 'Fellipe'; //Criando variável
-    console.log(nome, nome1);
+// if (verdadeiro) {
+//     let nome = 'Fellipe'; //Criando variável
+//     console.log(nome, nome1);
 
-    if (verdadeiro) {
-        let nome = 'John' //Criando outra variável
-        console.log(nome, nome1);
-    }
-}
+//     if (verdadeiro) {
+//         let nome = 'John' //Criando outra variável
+//         console.log(nome, nome1);
+//     }
+// }
+
+
+//Testando micro escopo 
+
+// function say() {
+//     let nome = 'Primeiro escopo'
+//     var sobrenome = 'Reis';
+
+//     if (verdadeiro) {
+//         let nome = 'Fellipe';
+//         console.log(nome,sobrenome) //as variaveis globais podem ser utilizadas dentro de qualquer micro bloco dentro da função, porem nenhuma variavel dos micro bloco podem ser usadas fora do escopo da função,  como exemplo vimos a Let nome = 'Fellipe'; que não pode ser usada fora do escopo do if()
+//     }
+
+//     console.log(nome,sobrenome)
+// }
+
+// say();
+
