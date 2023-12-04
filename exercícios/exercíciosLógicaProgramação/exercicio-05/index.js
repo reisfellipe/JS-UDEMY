c42d8265c8cd7f07410643983e6d27dabb5e3193
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2023-12-04 09:44:19 -0300

    exercising

--- exercícios/exercíciosLógicaProgramação/exercicio-05/index.js
+++ exercícios/exercíciosLógicaProgramação/exercicio-05/index.js
@@ -1,9 +1,12 @@
-const maiorValor = (x,y) => {
-    if (x > y) {
-        return x
-    }  else {
-        return y
-    }
-};
+// const maiorValor = (x,y) => {
+//     if (x > y) {
+//         return x
+//     }  else {
+//         return y
+//     }
+// };
+
+const maiorValor = (x,y) => x > y ? x : y;
+
 
 console.log(maiorValor(12,6));
---
+++ exercícios/exercíciosLógicaProgramação/exercicio-06/index.js
@@ -0,0 +1,3 @@
+const isLandscape = (width,height) => width > height ? true : false;
+
+console.log(isLandscape(3880,200));
