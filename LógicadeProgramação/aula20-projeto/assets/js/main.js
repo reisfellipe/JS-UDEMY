3dd8bba54abb987d63f320839cc600f679705612
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2023-12-05 20:55:58 -0300

    praticing enginees functions

--- LógicadeProgramação/aula20-projeto/assets/js/main.js
+++ LógicadeProgramação/aula20-projeto/assets/js/main.js
@@ -35,6 +35,7 @@ function criaTarefa(textoInput) {
     tarefas.appendChild(li);
     limpaInput();
     criaBotaoApagar(li);
+    salvarTarefas();
 }
 
 btnTarefa.addEventListener('click', function(){
@@ -45,6 +46,20 @@ btnTarefa.addEventListener('click', function(){
 document.addEventListener('click', function(e){
     const el = e.target;
     if(el.classList.contains('apagar')){
-        console.log(el.parentElement);
+        el.parentElement.remove();
     }
 });
+
+function salvarTarefas(){
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for(let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar Tarefa', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    console.log(listaDeTarefas);
+}
