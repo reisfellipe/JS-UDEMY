671257b02f1767ba626ec37c9a6f87177851eac1
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2023-12-08 18:37:17 -0300

    prcticing factory functions

---
+++ exercícios/exercíciosLógicaProgramação/exercicio-09/assets/js/main.js
@@ -0,0 +1,76 @@
+function criaCalculadora(){
+    
+    return {
+        display: document.querySelector('.display'),
+        
+        inicia(){
+            this.cliqueBotoes();
+            this.pressEnter();
+        },
+
+        pressEnter(){
+            this.display.addEventListener('keypress', e => {
+                if(e.keyCode === 13){
+                    return this.realizaConta();
+                }
+            });
+        },
+        
+        realizaConta(){
+             let conta = this.display.value;
+
+             try {
+                conta = eval(conta);
+                if(!conta){
+                    alert('Conta inválida');
+                    return;
+                }
+
+                this.display.value = String(conta);
+             }catch(e){
+                alert('Conta inválida');
+                return;
+             }  
+        },
+
+        clearDisplay(){
+            this.display.value =  '';
+        },
+
+        deleteOne(){
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+
+        cliqueBotoes(){
+            // this -> calculadora
+            document.addEventListener('click', e => {
+                const el = e.target;    
+
+                if(el.classList.contains('btn-num')){
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if(el.classList.contains('btn-clear')){
+                    this.clearDisplay();
+                }
+
+                if(el.classList.contains('btn-del')){
+                    this.deleteOne();
+                }
+
+                if(el.classList.contains('btn-eq')){
+                    this.realizaConta();
+                }
+            });
+        },
+
+        btnParaDisplay(valor){
+            this.display.value += valor;
+        }
+
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
