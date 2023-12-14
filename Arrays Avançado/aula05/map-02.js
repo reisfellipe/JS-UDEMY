69e3ac5487816f863fb40ae60166b85b9ca4f206
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2023-12-14 16:36:43 -0300

    map exercises

---
+++ Arrays Avançado/aula05/map-02.js
@@ -0,0 +1,29 @@
+// Para cada elemento:
+// Retorne apenas uma string com o nome da pessoa
+// Remova apenas a chave 'nome' do objeto
+// Adicione uma chave id em cada objeto
+
+const pessoas = [
+    {nome: 'Ledger', idade:28},
+    {nome: 'Kenna', idade:26},
+    {nome: 'Patrick', idade:66},
+    {nome: 'Grace', idade:59},
+    {nome: 'Diem', idade:5},
+    {nome: 'Roman', idade:29},    
+];
+
+const nomes = pessoas.map(obj => obj.nome);
+console.log(nomes); 
+
+const idades = pessoas.map(obj => {
+//  Uma forma de deletar a chave nome do objeto
+    // delete obj.nome;
+    // return obj;
+//Outra forma de fazer isso, é simplesmente retornar um objeto literal contendo só os valores da chave idade. Veja;
+    return {idade: obj.idade};
+});
+console.log(idades);
+
+// ou simplesmente fazer em uma linha
+const onlyIdades = pessoas.map(obj => ({idade: obj.idade}));
+console.log(onlyIdades);
---
+++ Arrays Avançado/aula05/map.js
@@ -0,0 +1,7 @@
+// Dobre os números
+// 
+
+const numeros = [5,30,50,12,3,80,35,90];
+const double = numeros.map(valor => valor * 2);
+console.log(double);
+
