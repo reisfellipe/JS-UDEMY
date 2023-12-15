5d024b790454fc10c812932954df59d26372d60d
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2023-12-15 15:50:24 -0300

    exercising reduce

--- Arrays Avançado/aula06/reduce-02.js
+++ Arrays Avançado/aula06/reduce-02.js
@@ -4,11 +4,27 @@
 // Papel de reduce() é reduzir um array em um único elemento
 
 const numeros = [5,50,80,1,2,3,5,8,7,1,15,22,77];
-const total = numeros.reduce((acumulador, valor, indice, array) => {
-if(valor % 2 === 0){
-
-}
-    return acumulador;
+const total = numeros.reduce((acumulador, valor) => {
+if(valor % 2 === 0) acumulador.push(valor);
+return acumulador;
 }, []);
 
-console.log(total);
+// console.log(total);
+
+// Retorne a pessoa mais velha
+
+const pessoas = [
+    {nome: 'Ledger', idade: 28},
+    {nome: 'Kenna', idade: 25},
+    {nome: 'Diem', idade: 5},
+    {nome: 'Scotty', idade: 21},
+    {nome: 'Patrick', idade: 59},
+    {nome: 'Grace', idade: 54},
+];
+
+const maisVelha = pessoas.reduce(function(acumulador, valor){
+    if(acumulador.idade > valor.idade) return acumulador;
+    return valor;
+});
+
+console.log(maisVelha);
