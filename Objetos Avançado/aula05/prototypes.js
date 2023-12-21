13c5cf81fc91d2c48d0077ae08e99af364a2a771
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2023-12-20 21:24:24 -0300

    learning prototype

--- Objetos Avançado/aula05/prototypes.js
+++ Objetos Avançado/aula05/prototypes.js
@@ -7,18 +7,27 @@ Protótipo é o termo usado para se referir ao que foi criado pela primeira vez,
 Todos os objetos tem uma referência interna para um protótipo (__proto__), que vem da propriedade prototype da função construtora que foi usada para criá-lo. Quando tentamos acessar este membro no próprio objeto e depois a cadeia de protótipos é usada até o topo (null) até encontrar (ou não) tal membro.
 */
 
-
-
 // Construtora -> molde (classe)
 function Pessoa(nome, sobrenome){
     this.nome = nome;
     this.sobrenome = sobrenome;
-    this.nomeCompleto = () => this.nome + ' ' + this.sobrenome;
+    // this.nomeCompleto = () => "ORIGINAL: " + this.nome + ' ' + this.sobrenome;
 }
+
+// Pessoa.prototype === pessoa1.__proto__
+
+Pessoa.prototype.nomeCompleto = function() {
+    return this.nomeCompleto = () => this.nome + ' ' + this.sobrenome;
+
+};
 
 // Instância
 const pessoa1 = new Pessoa('Ledger', 'Ward'); // <- Pessoa = Função Construtora
 const pessoa2 = new Pessoa('Ryle', 'Kincaid'); // <- Pessoa = Função Construtora
 
-console.log(pessoa1);
-console.log(pessoa2); 
+const data = new Date(); // <- Date = Função construtora
+
+console.log(Pessoa);
+// console.log(pessoa1);
+// console.log(pessoa2);
+console.log(data); 
