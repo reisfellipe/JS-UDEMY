01c944b050db56e17801b7d5eeb10c1d1b5ac265
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2023-12-21 22:15:05 -0300

    herança

--- Objetos Avançado/aula07/index.js
+++ Objetos Avançado/aula07/index.js
@@ -20,9 +20,34 @@ function Camiseta(nome, preco, cor){
 Camiseta.prototype = Object.create(Produto.prototype);
 Camiseta.prototype.constructor = Camiseta;
 
+Camiseta.prototype.aumento = function(percetual){
+    this.preco = this.preco + (this.preco * (percetual /100));
+}
+
+function Caneca(nome, preco, material, estoque){
+    Produto.call(this, nome, preco);
+    this.material = material;
+    
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        configurable: false,
+        get: function(){
+            return estoque;
+        },
+        set: function(value){
+            if(typeof value !== 'number') return;
+            estoque = value;
+        }
+    });
+}
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
 const produto = new Produto('Gen', 111);
 const camiseta = new Camiseta('Regata', 7.5, 'Preta');
+const caneca = new Caneca('Caneca Premium', 15, 'Plástico', 5);
 console.log(produto);
 console.log(camiseta);
+console.log(caneca);
 
 
