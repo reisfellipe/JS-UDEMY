ed6e669a33506752c857577cdfebb1585b238f34
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2023-12-24 17:56:21 -0300

    processing CPF

--- Objetos Avançado/aula08/checkCPF.js
+++ Objetos Avançado/aula08/checkCPF.js
@@ -14,4 +14,42 @@ Se o número do dígito for maior que 9, consideramos 0.
 11 - (284 % 11) = 5 (Primeiro dígito)
 */
 
- 
+function ValidaCPF(cpfEnviado){
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function(){
+            return cpfEnviado.replace(/\D+/g, '');
+        }
+    });
+}
+
+ValidaCPF.prototype.valida = function(){
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+    
+    const novoCpf = cpfParcial + digito1 + digito2;
+    console.log(novoCpf);
+
+    return novoCpf === this.cpfLimpo;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial){
+    const cpfArray = Array.from(cpfParcial);
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, value)=>{
+        ac += (regressivo * Number(value));
+        regressivo--;
+        return ac;
+    }, 0);
+    
+    const digito = 11 - (total % 11);
+    return digito > 9 ? 0 : String(digito);
+};
+
+const cpf = new ValidaCPF('070.987.720-03');
+console.log(cpf.valida())
+// cpf.valida();
