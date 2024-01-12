e5ab9f46f8e4a818a4911e57bb1b1ec1dd5b8113
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2024-01-11 23:53:31 -0300

    requisição XHR

--- JavaScript Assíncrono/XMLhttpRequest/modeloHTML/assets/js/main.js
+++ JavaScript Assíncrono/XMLhttpRequest/modeloHTML/assets/js/main.js
@@ -1,15 +1,17 @@
 const request = obj => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true);
-    xhr.send();
-
-
-    xhr.addEventListener('load', ()=>{
-        if(xhr.status >= 200 && xhr.status < 300){
-            obj.success(xhr.responseText);
-        }else {
-            obj.error(xhr.statusText);
-        }
+    return new Promise((resolve,reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send();
+    
+    
+        xhr.addEventListener('load', ()=>{
+            if(xhr.status >= 200 && xhr.status < 300){
+                resolve(xhr.responseText);
+            }else {
+                reject(xhr.statusText);
+            }
+        });
     });
 };
 
