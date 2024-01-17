5eddfd72051590ba082e58c7cab8f575d28ae36d
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2024-01-17 20:56:49 -0300

    fetch api

--- JavaScript Assíncrono/XMLhttpRequest/modeloHTML/assets/js/main.js
+++ JavaScript Assíncrono/XMLhttpRequest/modeloHTML/assets/js/main.js
@@ -1,19 +1,19 @@
-const request = obj => {
-    return new Promise((resolve,reject)=>{
-        const xhr = new XMLHttpRequest();
-        xhr.open(obj.method, obj.url, true);
-        xhr.send();
+// const request = obj => {
+//     return new Promise((resolve,reject)=>{
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(obj.method, obj.url, true);
+//         xhr.send();
     
     
-        xhr.addEventListener('load', ()=>{
-            if(xhr.status >= 200 && xhr.status < 300){
-                resolve(xhr.responseText);
-            }else {
-                reject (xhr.statusText);
-            }
-        });
-    });
-};
+//         xhr.addEventListener('load', ()=>{
+//             if(xhr.status >= 200 && xhr.status < 300){
+//                 resolve(xhr.responseText);
+//             }else {
+//                 reject (xhr.statusText);
+//             }
+//         });
+//     });
+// };
 
 document.addEventListener('click', e => {
     const el = e.target;
@@ -26,28 +26,50 @@ document.addEventListener('click', e => {
 });
 
 async function carregaPagina(el){
-    const href = el.getAttribute('href');
-    console.log(href);
-    const objConfig = {
-        method: 'GET',
-        url: href,
-        success(response){
-            carregaResultado(response);
-        },
-        error(errorText){
-            console.log(errorText);
-        }
-    };
-
     try{
-        const response = await request(objConfig);
-        carregaResultado(response);
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+        
+        if(response.status !== 200) throw new Error('ERROR 404 CREATED FROM JS');
+    
+        const html = await response.text();
+        carregaResultado(html);
     }catch(e){
         console.log(e);
     }
+
+    // fetch(href)
+    //     .then(response => {
+    //         if(response.status !== 200) throw new Error('ERROR 404 CREATED FROM JS');
+    //         return response.text();
+    //     })
+    //     .then(html => carregaResultado(html))
+    //     .catch(e => console.log(e));
+        
+    // const objConfig = {
+    //     method: 'GET',
+    //     url: href,
+    //     success(response){
+    //         carregaResultado(response);
+    //     },
+    //     error(errorText){
+    //         console.log(errorText);
+    //     }
+    // };
+
 }
 
 function carregaResultado(response){
     const resultado = document.querySelector('.resultado');
     resultado.innerHTML = response;
 };
+
+//FETCH API 
+
+// fetch('pagina-6.html')
+//     .then(resposta => {
+//         if(resposta.status !== 200) throw new Error('ERROR 404 NOSSO');
+//         return resposta.text();
+//     })
+//     .then(html => console.log(html))
+//     .catch(e => console.warn(e));
