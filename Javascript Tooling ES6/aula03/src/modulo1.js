571a2e19f0ee6f110c0aef3aa4d3d2dd88929fc4
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2024-01-27 22:50:54 -0300

    copying files

---
+++ Javascript Tooling ES6/aula03/src/modulo1.js
@@ -0,0 +1,20 @@
+export const nome = 'Fellipe'; //exportando e criando ao mesmo tempo
+export const sobrenome = 'Reis';
+export const idade = 19;
+
+export default function soma(x,y){
+    return x + y;
+}
+
+// export function combinaNomes(nome1,nome2){
+//     return String(nome1 + nome2);
+// }
+
+// export class Pessoa{
+//     constructor(nome,sobrenome){
+//         this.nome = nome;
+//         this.sobrenome = sobrenome;
+//     }
+// }
+
+// export {nome, sobrenome, idade, soma};
