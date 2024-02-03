3b730bd96388982f80059fbe1b13f2349823ad5a
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2024-02-02 21:13:53 -0300

    create mod1

---
+++ node/aula01/mod2.js
@@ -0,0 +1,12 @@
+class Pessoa {
+    constructor(nome){
+        this.nome = nome;
+    }
+}
+
+const nome = 'Fellipe';
+const sobrenome = 'Reis';
+
+module.exports = {
+    nome,sobrenome,Pessoa
+};
