59f1ac5e459a52c2a7d1368c6e4a1c6b0959b361
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2024-02-05 20:19:02 -0300

    making example

---
+++ node/aula2/app.js
@@ -0,0 +1,8 @@
+// const multiplicacao = require('./mod');
+
+// console.log(multiplicacao(2,5));
+
+const cachorrinho = require('./B/C/D/mod');
+const jeff = new cachorrinho('Jeff');
+
+jeff.latir();
