f3e3f20761f275db747da6e647bd8514a111dddd
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2024-02-08 21:45:25 -0300

    learn use fs

--- node/aula4/index.js
+++ node/aula4/index.js
@@ -1,6 +1,37 @@
 const fs = require('fs').promises;
+const { stat } = require('fs');
 const path = require('path');
 
-fs.readdir('./')
-    .then(files => console.log(files))
-    .catch(e => console.log(e));
+async function readdir(rootDir){
+    rootDir = rootDir || path.resolve(__dirname);
+    const files = await fs.readdir(rootDir);
+    walk(files,rootDir);
+}
+
+async function walk(files, rootDir){
+    for(let file of files){
+        const fileFullPath = path.resolve(rootDir,file);
+        const stats = await fs.stat(fileFullPath); 
+        
+        if(/\.git/g.test(fileFullPath)) continue;
+        if(/node_modules/g.test(fileFullPath)) continue;
+        
+        if(stats.isDirectory()){
+            readdir(fileFullPath);
+            continue;
+        }
+        
+        if(
+            !/\.css$/g.test(fileFullPath) && !/\.html$/g.test(fileFullPath) 
+            ) continue;
+
+        console.log(fileFullPath);
+    }
+}
+
+//para listar os diretorios dentro das pastas use a função criada 
+readdir('C:/Users/DELL/OneDrive/Documentos/Estudos Fellipe/JS-UDEMY/JS-UDEMY');
+
+// fs.readdir(path.resolve(__dirname))
+//     .then(files => console.log(files))
+//     .catch(e => console.log(e));
