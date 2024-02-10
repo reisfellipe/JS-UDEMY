477f140fb733e4aabc8cb6f858945a584ef8613e
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2024-02-09 22:26:28 -0300

    done

--- node/aula5/apps.js
+++ node/aula5/apps.js
@@ -1,13 +1,27 @@
 const path = require('path');
 const caminhoArquivo = path.resolve(__dirname, 'teste.json');
 const escreve = require('./modules/escrever');
+const ler = require('./modules/ler');
 
-const pessoas = [
-    {nome: 'Peter'},
-    {nome: 'Chris'},
-    {nome: 'Ledger'},
-    {nome: 'Marshall'},
-    {nome: 'Atlas'},
-];
-const json = JSON.stringify(pessoas, '', 2);
-escreve(caminhoArquivo, json);
+// const pessoas = [
+//     {nome: 'Peter'},
+//     {nome: 'Chris'},
+//     {nome: 'Ledger'},
+//     {nome: 'Marshall'},
+//     {nome: 'Atlas'},
+// ];
+// const json = JSON.stringify(pessoas, '', 2);
+// escreve(caminhoArquivo, json);
+
+async function leArquivo(caminho){
+    const dados = await ler(caminho);
+    renderizaDados(dados);
+}
+
+function renderizaDados(dados){
+    dados = JSON.parse(dados);
+    dados.forEach(val => console.log(val.nome));
+}
+
+leArquivo(caminhoArquivo);
+
