b40671f1b2611ff589a2eb0dcd532011beda234b
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2024-02-09 23:28:36 -0300

    update

---
+++ node/aula6/server.js
@@ -0,0 +1,8 @@
+const express  = require('express');
+const app = express();
+
+//          Criar  Ler   Atualizar  Apagar
+//  CRUD -> CREATE READ  UPDATE     DELETE
+//          POST   GET   PUT        DELETE
+
+//http://meusite.com/ <- GET -> Entregue a página 
