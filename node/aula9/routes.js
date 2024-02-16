f0a268b6abc6f69a0868ca7eb7dff10cf7f9d00e
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2024-02-15 22:11:37 -0300

    step 5

---
+++ node/aula9/routes.js
@@ -0,0 +1,14 @@
+//A function Router() entende qual rota que é e busca um controlador vai decidir qual view e qual model a gente vai utilizar
+
+const express = require('express');
+const route = express.Router();
+const homeController = require('./controllers/homeController');
+const contatoController = require('./controllers/contatoController');
+
+//rotas da home
+route.get('/', homeController.paginaInicial);
+route.post('/', homeController.trataPost);
+// Rotas de contato
+route.get('/contato', contatoController.paginaInicial);
+
+module.exports = route;
