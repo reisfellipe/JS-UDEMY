68b7e7f52bf21a4b90413516bc25cfef65209bcd
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2024-02-15 22:30:40 -0300

    step 3

--- node/aula10/server.js
+++ node/aula10/server.js
@@ -1,8 +1,11 @@
 const express  = require('express');
 const app = express();
 const routes = require('./routes');
+const path = require('path');
 
 app.use(express.urlencoded({extended:true}));
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs'); //Vamos usar essa engine para tratar dados no html, que no html só não é possível
 app.use(routes);
 
 app.listen(3000, ()=>{
