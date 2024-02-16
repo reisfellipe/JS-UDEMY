d60068ace4a36602c0d98c4254727340f8207265
Fellipe <125813498+reisfellipe@users.noreply.github.com>
2024-02-15 22:30:25 -0300

    step 1 to learn view ejs

--- node/aula10/routes.js
+++ node/aula10/routes.js
@@ -2,8 +2,8 @@
 
 const express = require('express');
 const route = express.Router();
-const homeController = require('./controllers/homeController');
-const contatoController = require('./controllers/contatoController');
+const homeController = require('./src/controllers/homeController');
+const contatoController = require('./src/controllers/contatoController');
 
 //rotas da home
 route.get('/', homeController.paginaInicial);
