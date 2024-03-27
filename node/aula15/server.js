8ed48899b98219e418abd531dabf48810fcf51f4
Fellipe Reis <reisfellipe11@gmail.com>
2024-03-26 23:23:05 -0300

    saving

--- node/aula15/server.js
+++ node/aula15/server.js
@@ -2,23 +2,16 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
-// const connectionString = 'mongodb+srv://justfellipe:asenhaesenha@cluster0.bk5ee3g.mongodb.net/BASEDEDADOS?retryWrites=true&w=majority&appName=Cluster0';
+
 mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log('Conectado ao MongoDB');
-        app.listen(3000, () => {
-            console.log("Acessar http://localhost:3000");
-            console.log('Servidor executando na porta 3000');
-        });
+        app.emit('pronto');
     })
     .catch(e => console.log(e));
 
-
-
 const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 
 const routes = require('./routes');
@@ -26,11 +19,12 @@ const path = require('path');
 const { middlewareGlobal } = require('./src/middlewares/middleware');
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
-    secret: ' kbhcuiebfoief lkjsdncponcpiefm',
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
     resave: false,
     saveUninitialized: false,
     cookie: {
@@ -41,18 +35,19 @@ const sessionOptions = session({
 app.use(sessionOptions);
 app.use(flash());
 
-
 app.set('views', path.resolve(__dirname, 'src', 'views'));
-app.set('view engine', 'ejs'); //Vamos usar essa engine para tratar dados no html, que no html só não é possível
+app.set('view engine', 'ejs');
 
-//Nossos próprios middlewares
-app.use(middlewareGlobal); //Assim garantimos que todas as requisições, rotas passem pelo middlewares antes
+// Nossos próprios middlewares
+app.use(middlewareGlobal);
 app.use(routes);
-// app.on('Conectado agora!', () => {
-//     app.listen(3000, () => {
-//         console.log("Acessar http://localhost:3000");
-//         console.log('Servidor executando na porta 3000');
-//     });
-// });
 
-//comentário qualquer
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        console.log('Conectado ao MongoDB');
+        app.listen(3000, () => {
+            console.log("Acessar http://localhost:3000");
+            console.log('Servidor executando na porta 3000');
+        });
+    })
+    .catch(e => console.log(e));
--- node/aula15/src/controllers/homeController.js
+++ node/aula15/src/controllers/homeController.js
@@ -1,13 +1,18 @@
-const HomeModel = require('../models/HomeModel');
+// const HomeModel = require('../models/HomeModel');
 
-HomeModel.create({
-    titulo:'Um titulo de testes',
-    descricao:'Uma descricao de testes;'
-})
-.then(dados => console.log(dados))
-.catch(e => console.log(e));
+// HomeModel.create({
+//     titulo:'Um titulo de testes',
+//     descricao:'Uma descricao de testes;'
+// })
+// .then(dados => console.log(dados))
+// .catch(e => console.log(e));
+
 
 exports.paginaInicial = (req, res) => {
+    req.session.usuario = {nome: 'Fellipe', logado:true};
+    req.flash('info', 'Primeira flash messagem');
+    req.flash('error', 'Deu erro ops');
+    req.flash('success', 'Agora deu certo');
     res.render('index');
     return;
 };
