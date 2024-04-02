404a8459bb6be0db28f734546aa771eb0180f2cc
Fellipe Reis <reisfellipe11@gmail.com>
2024-04-01 22:23:29 -0300

    installed helmet

---
+++ node/aula17/src/middlewares/middleware.js
@@ -0,0 +1,18 @@
+exports.middlewareGlobal = (req, res, next) => {
+    next();
+};
+
+exports.outroMiddleware = (req, res, next) => {
+    next();
+};
+
+exports.checkCsrfError = (err, req, res, next) =>{
+    if(err & err.code === 'EBADCSRFTOKEN'){
+        return res.render('404');
+    }
+};
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+};
