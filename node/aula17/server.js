549bd5e40f8137cd4fea687a5671157c554d5f85
Fellipe Reis <reisfellipe11@gmail.com>
2024-04-01 22:36:44 -0300

    get finish

--- node/aula17/server.js
+++ node/aula17/server.js
@@ -17,8 +17,11 @@ const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
 app.use(helmet());
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
+
 const sessionOptions = session({
     secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
     store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
@@ -30,7 +33,9 @@ const sessionOptions = session({
     }
 });
 app.use(sessionOptions);
+
 app.use(flash());
+
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
@@ -40,6 +45,7 @@ app.use(middlewareGlobal);
 app.use(checkCsrfError);
 app.use(csrfMiddleware);
 app.use(routes);
+
 mongoose.connect(process.env.CONNECTIONSTRING)
     .then(() => {
         console.log('Conectado ao MongoDB');
