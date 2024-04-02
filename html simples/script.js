4af6a974e1abf114f5c7583ce61e682c171221c1
Fellipe Reis <reisfellipe11@gmail.com>
2024-04-01 21:44:16 -0300

    update class

---
+++ html simples/script.js
@@ -0,0 +1,7 @@
+const contato = document.getElementById('contatoBotao');
+
+contato.addEventListener('click', () =>{
+    console.log('o botão foi clicado');
+    document.alert('Iremos entrar em contato em breve!');
+})
+
--- marcia.js
+++
@@ -1,6 +0,0 @@
-const nome = 'Marcia';
-const sobrenome = 'Aparecida';
-const ultimoNome = 'Carvalho';
-
-console.log(nome, sobrenome, ultimoNome);
-
