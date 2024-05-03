eb11e5e2022ea56382f1b89c1b8261f33aa32284
Fellipe Reis <reisfellipe11@gmail.com>
2024-05-02 21:25:45 -0300

    saving

--- projetoagenda/server.js
+++ projetoagenda/server.js
@@ -14,7 +14,7 @@ const routes = require('./routes');
 const path = require('path');
 const helmet = require('helmet');
 const csrf = require('csurf');
-const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
+const { middlewareGlobal, checkCsrfError,c csrfMiddleware } = require('./src/middlewares/middleware');
 
 app.use(helmet());
 
